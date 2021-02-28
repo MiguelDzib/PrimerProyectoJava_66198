b25fde239440fec2ecd0fa1a8149615974200d84
galbe <galbe@192.168.1.68>
2021-02-28 10:05:13 -0600

    Primer proyecto java 66198

---
+++ src/primerproyectojava_66198/PrimerProyectoJava_66198.java
@@ -0,0 +1,14 @@
+
+package primerproyectojava_66198;
+
+public class PrimerProyectoJava_66198 {
+
+    public static void main(String[] args) {
+       
+        int alumnos= 30 ;
+        char grupo= 'A' ;
+        boolean carreraSistemas= true ;     
+        
+    }
+    
+}
